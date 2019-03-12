c13027adaf82201ccc2d2a847ccea58bca6b7095
Windows 10 <Windows 10@172.18.173.92>
2019-03-12 16:54:43 +0100

    Code Cleanup

--- src/domein/Tegel.java
+++ src/domein/Tegel.java
@@ -2,46 +2,25 @@ package domein;
 
 
 
-public class Tegel
-{
+public class Tegel {
     int nummer;
     int waarde;
 
-    public Tegel(int nummer) 
-    {
+    public Tegel(int nummer) {
         this.nummer = nummer;
         berekenwaarde();
         
     }
-    public void berekenwaarde()
-    {
-        if (nummer >= 21 && nummer <= 24) 
-        {
+    public void berekenwaarde() {
+        if (nummer >= 21 && nummer <= 24) {
             waarde = 1;
-        }
-        else
-        {
-            if (nummer >=25 && nummer <= 28) 
-            {
+        } else if (nummer >=25 && nummer <= 28) {
                 waarde = 2;
-            }
-            else
-            {
-                if (nummer>=29 && nummer <= 32) 
-                {
+            } else if (nummer>=29 && nummer <= 32) {
                  waarde = 3;
-                }
-                else
-                {
-                    if (nummer >= 33 && nummer <=36) 
+                } else if (nummer >= 33 && nummer <=36) 
                     {
                         waarde = 4;
                     }
                 }
             }
-        }
-        
-    }
-
-    
-}
